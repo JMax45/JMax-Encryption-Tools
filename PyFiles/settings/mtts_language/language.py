cae128640a27e1eca986b416fcede1ab013058e5
JMax45 <36378436+JMax45@users.noreply.github.com>
2018-12-26 11:12:12 +0100

    Add files via upload
    Added new languages to gtts: Hindi, Spanish, Arabic, Russian, Bengali, Portuguese, Indonesian, Français

--- PyFiles/settings/mtts_language/language.py
+++ PyFiles/settings/mtts_language/language.py
@@ -4,3 +4,28 @@ def l_en_US():
 def l_it_IT():
     f = open("PyFiles/settings/mtts_language/language.txt","w")
     f.write("it")
+def l_hi_IN():
+    f = open("PyFiles/settings/mtts_language/language.txt","w")
+    f.write("hi")    
+def l_es_ES():
+    f = open("PyFiles/settings/mtts_language/language.txt","w")
+    f.write("es")
+def l_ar_IL():
+    f = open("PyFiles/settings/mtts_language/language.txt","w")
+    f.write("ar")
+def l_ru_RU():
+    f = open("PyFiles/settings/mtts_language/language.txt","w")
+    f.write("ru")
+def l_bn_BD():
+    f = open("PyFiles/settings/mtts_language/language.txt","w")
+    f.write("bn")
+def l_pt_PT():
+    f = open("PyFiles/settings/mtts_language/language.txt","w")
+    f.write("pt")     
+def l_id_ID():
+    f = open("PyFiles/settings/mtts_language/language.txt","w")
+    f.write("id")
+def l_fr_FR():
+    f = open("PyFiles/settings/mtts_language/language.txt","w")
+    f.write("fr")      
+    
